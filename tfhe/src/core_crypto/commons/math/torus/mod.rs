eca42221be3d4d93547e26f1a9cdedc5dbcb0271
Arthur Meyre <arthur.meyre@zama.ai>
2023-11-14 16:48:03 +0100

    fix(core): noise gen fix for modulus approx

--- tfhe/src/core_crypto/commons/math/torus/mod.rs
+++ tfhe/src/core_crypto/commons/math/torus/mod.rs
@@ -12,7 +12,6 @@
 //! traits which allow to go back and forth between an unsigned integer representation and a
 //! floating point representation.
 
-use crate::core_crypto::algorithms::misc::convert_unsigned_integer_to_float_truncate;
 use crate::core_crypto::commons::math::random::{
     Gaussian, RandomGenerable, Uniform, UniformBinary, UniformTernary,
 };
@@ -79,20 +78,7 @@ macro_rules! implement {
             }
             #[inline]
             fn from_torus_custom_mod(input: F, custom_modulus: Self) -> Self {
-                // Reasonging with F = f64
-                //
-                // Here we convert the custom modulus to f64 but rounding down to the closest
-                // representable value by f64. This allows to make sure that the value that
-                // is outputed is in the correct range with respect to our modulus.
-                // The max error for a u64 is 2048, i.e. max(diff(floor(u64 as f64) as u64, u64)) <
-                // 2048 This small error should be acceptable for values produced by the
-                // gaussian generation as they have to be related to the modulus itself,
-                // 2048 / 2^63 = 2^-52, basically the f64 relative error is the max relative
-                // error you can get. This is valid for other moduli >= 2^53 with a max
-                // absolute error as low as 1 e.g. for 2^64 - 2^32 + 1, which rounds down to
-                // 2^64 - 2^32 in the float domain.
-                let custom_modulus_float: F =
-                    convert_unsigned_integer_to_float_truncate(custom_modulus);
+                let custom_modulus_float: F = custom_modulus.cast_into();
 
                 // This is in [-0.5, 0.5[
                 // We do not do the mapping to [0, 1[ here as some values can be extremely small
